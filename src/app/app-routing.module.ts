cd68fcac9b044c63e8c494431d4723c4df4f2530
Rober <boblawyer1@gmail.com>
2022-05-12 19:19:35 -0300

    agregada la navegacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BannerComponent } from './banner/banner.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'about', component: AboutComponent},
+  {path: 'banner', component: BannerComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'skills', component: SkillsComponent},
+  {path: '', redirectTo: '/banner', pathMatch: 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
